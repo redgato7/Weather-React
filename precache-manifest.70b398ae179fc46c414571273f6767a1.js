aa16f2bf5901d5ee9609548882c30afa9f7829fb
redgato7 <dawidzebacki>
2020-01-12 21:57:44 +0100

    Updates

---
+++ precache-manifest.70b398ae179fc46c414571273f6767a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d190e20f1c2c87466e665cbc8abca6",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "6e61d17933c5083ef5ab",
+    "url": "/Weather-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5f60b427defef18582ec",
+    "url": "/Weather-React/static/css/main.0e365848.chunk.css"
+  },
+  {
+    "revision": "6e61d17933c5083ef5ab",
+    "url": "/Weather-React/static/js/2.8283ac2c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Weather-React/static/js/2.8283ac2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f60b427defef18582ec",
+    "url": "/Weather-React/static/js/main.db49ebfe.chunk.js"
+  },
+  {
+    "revision": "567eb8e925c88476ca9c",
+    "url": "/Weather-React/static/js/runtime-main.5397020d.js"
+  },
+  {
+    "revision": "a41aa334fec0125aae2274f64c4e819c",
+    "url": "/Weather-React/static/media/gauge.a41aa334.svg"
+  },
+  {
+    "revision": "fc17916825fb428e780884d18bd753a3",
+    "url": "/Weather-React/static/media/sky_night.fc179168.jpg"
+  },
+  {
+    "revision": "0950346d4d3d923760aae26f205d62d2",
+    "url": "/Weather-React/static/media/windy.0950346d.svg"
+  }
+]);
